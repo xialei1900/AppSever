7cf5e5dd6d95ce9987884d2aee250472605c92dd
xialei1900 <574892525@qq.com>
2017-05-19 21:51:50 +0800

    second commit
    do some test

---
+++ AppSever/Common/StringHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Common
+{
+    class StringHelper
+    {
+        #region unicode 字符转义
+        /// <summary>  
+        ///  unicode 解码 
+        /// </summary>  
+        /// <param name="str"></param>  
+        /// <returns></returns>  
+        public static string UnicodeDencode(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+            return Regex.Unescape(str);
+        }
+        /// <summary>  
+        /// 将字符串进行 unicode 编码  
+        /// </summary>  
+        /// <param name="str"></param>  
+        /// <returns></returns>  
+        public static string UnicodeEncode(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+            StringBuilder strResult = new StringBuilder();
+            if (!string.IsNullOrEmpty(str))
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    strResult.Append("\\u");
+                    strResult.Append(((int)str[i]).ToString("x4"));
+                }
+            }
+            return strResult.ToString();
+        }
+        #endregion
+    }
+}
--- AppSever/Controllers/HomeController.cs
+++ AppSever/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ using System.Net;
 using System.IO;
 using System.Text;
 using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AppSever.Controllers
 {
@@ -40,11 +42,32 @@ namespace AppSever.Controllers
                 case "getRemindToday":
                     result = getRemindToday();
                     break;
-                case "getData":
-                    result = getData();
+                //case "getData":
+                //    result = getData();
+                //    break;
+                //case "getDataByself":
+                //    result = getDataByself();
+                //    break;
+                //case "getFood":
+                //    result = getFood();
+                //    break;
+                case "getFoodList":
+                    result = getFoodList();
                     break;
-                case "getDataByself":
-                    result = getDataByself();
+                case "updateRemind":
+                    result = updateRemind();
+                    break;
+                case "getItemList":
+                    result = getItemList();
+                    break;
+                case "getCheckList":
+                    result = getCheckList();
+                    break;
+                case "searchFoodByName":
+                    result = searchFoodByName();
+                    break;
+                case "getQuestionList":
+                    result = getQuestionList();
                     break;
             }
             return result;
@@ -82,10 +105,9 @@ namespace AppSever.Controllers
             string registerPwd = request.Form["registerPwd"].Trim();
             string registerUserName = request.Form["registerUserName"].Trim();
 
-
-            int state = sqlHelper.ExecuteSql("insert into Users (userName,loginId,loginPwd) values ('" + registerUserName + "'," + registerId + ",'" + registerPwd + "')");
+            int state = sqlHelper.ExecuteSql("insert into Users (userName,loginId,loginPwd) values ('"
+                + registerUserName + "'," + registerId + ",'" + registerPwd + "')");
             if (state > 0)
-                //return Content("Id:" + registerId + " 密码:" + registerPwd + " 名称:" + registerUserName);
                 return Content("S");
             else
                 return Content("E");
@@ -107,14 +129,31 @@ namespace AppSever.Controllers
                 week = count / 7;       //当前怀孕周数
                 day = count % 7 + 1;    //该周怀孕天数
             }
-            else {
+            else
+            {
                 day = count;
             }
 
-            DataSet ds = sqlHelper.GetDataSet("select babyPhysical,momPhysical from remindDailyData where week="+week+" and day="+day+"");
-            return Content(Common.JsonHelper.ToJson(ds.Tables[0]));
-            
-           
+            DataSet ds_1 = sqlHelper.GetDataSet("select top(1) babyPhysical,momPhysical from remindDailyData where week=" + week + " and day=" + day);
+            DataSet ds_2 = sqlHelper.GetDataSet("select top(1) checkTimes,checkWeek from pregnancyCheck where checkWeek>=" + week);
+            DataSet ds_3 = sqlHelper.GetDataSet("select top(1) nutritionRecipes from recipes where week=" + week + " and day=" + day);
+
+            DataTable table = ds_1.Tables[0];
+            table.Columns.Add("week");
+            table.Columns.Add("day");
+            table.Columns.Add("checkTimes");
+            table.Columns.Add("checkWeek");
+            table.Columns.Add("nutritionRecipes");
+
+            table.Rows[0]["week"] = week;
+            table.Rows[0]["day"] = day;
+            table.Rows[0]["checkTimes"] = ds_2.Tables[0].Rows[0]["checkTimes"];
+            table.Rows[0]["checkWeek"] = ds_2.Tables[0].Rows[0]["checkWeek"];
+            table.Rows[0]["nutritionRecipes"] = ds_3.Tables[0].Rows[0]["nutritionRecipes"];
+
+            return Content(Common.JsonHelper.ToJson(table));
+
+
         }
         #endregion
 
@@ -138,19 +177,19 @@ namespace AppSever.Controllers
             //解析html
             HtmlDocument doc = new HtmlDocument();
             doc.LoadHtml(html);
-            HtmlNode node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/dl[1]/dd[1]");
-            babyPhysical = node.InnerText.Trim();
-            node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/dl[1]/dd[2]");
-            momPhysical = node.InnerText.Trim();
-            node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/div[4]/dl");
+            //HtmlNode node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/dl[1]/dd[1]");
+            //babyPhysical = node.InnerText.Trim();
+            //node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/dl[1]/dd[2]");
+            //momPhysical = node.InnerText.Trim();
+            HtmlNode node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/div[4]/dl");
             nutritionRecipes = node.InnerText.Trim();
             //保存至数据库
-            int state = sqlHelper.ExecuteSql("insert into dailyData (week,day,babyPhysical,momPhysical,nutritionRecipes) values ("+week+","+day+",'" + babyPhysical + "','" + momPhysical + "','" + nutritionRecipes + "')");
+            //int state = sqlHelper.ExecuteSql("insert into dailyData (week,day,babyPhysical,momPhysical,nutritionRecipes) values ("+week+","+day+",'" + babyPhysical + "','" + momPhysical + "','" + nutritionRecipes + "')");
 
-            if (state < 1)
-                return Content("E");
-            else
-                return Content("S");
+            //if (state < 1)
+            //    return Content("E");//
+            //else
+            return Content(nutritionRecipes);
         }
         #endregion
 
@@ -158,8 +197,9 @@ namespace AppSever.Controllers
         public ContentResult getDataByself()
         {
             string html;
-            string babyPhysical ="";
+            string babyPhysical = "";
             string momPhysical = "";
+            string nutritionRecipes = "";
             bool flag = true;
             string week = request.Form["week"].Trim();
 
@@ -174,14 +214,15 @@ namespace AppSever.Controllers
                 html = streamReader.ReadToEnd();
                 //解析html
                 doc.LoadHtml(html);
-                HtmlNode node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/dl[1]/dd[1]");
-                babyPhysical = node.InnerText.Trim();
-                node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/dl[1]/dd[2]");
-                momPhysical = node.InnerText.Trim();
-                //node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/div[4]/dl");
-                //nutritionRecipes = node.InnerText.Trim();
+                //HtmlNode node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/dl[1]/dd[1]");
+                //babyPhysical = node.InnerText.Trim();
+                //node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/dl[1]/dd[2]");
+                //momPhysical = node.InnerText.Trim();
+                HtmlNode node = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/aside/div[4]/dl");
+                nutritionRecipes = node.InnerText.Trim();
                 //保存至数据库
-                int state = sqlHelper.ExecuteSql("insert into remindDailyData (week,day,babyPhysical,momPhysical) values (" + week + "," + day + ",'" + babyPhysical + "','" + momPhysical + "')");
+                //int state = sqlHelper.ExecuteSql("insert into remindDailyData (week,day,babyPhysical,momPhysical) values (" + week + "," + day + ",'" + babyPhysical + "','" + momPhysical + "')");
+                int state = sqlHelper.ExecuteSql("insert into recipes (week,day,nutritionRecipes) values (" + week + "," + day + ",'" + nutritionRecipes + "')");
                 //w = week;
                 //d = day;
                 if (state < 1)
@@ -192,6 +233,135 @@ namespace AppSever.Controllers
                 return Content("S");
             else
                 return Content("E");
+        }
+        #endregion
+
+        #region MyRegion
+        public ContentResult getFood()
+        {
+            //获取html
+            int j = 0;
+            for (int i = 558; i <= 572; i++)
+            {
+                HttpWebRequest rq = (HttpWebRequest)HttpWebRequest.Create("http://www.mama.cn/index.php?g=Home&a=QrqmAjax&d=safeFoodsById&id=" + i + "&age=1");    //创建一个请求示例
+                rq.Method = "Get";
+                HttpWebResponse response = (HttpWebResponse)rq.GetResponse();　　//获取响应，即发送请求
+                Stream responseStream = response.GetResponseStream();
+                StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8);
+                string jsonResult = streamReader.ReadToEnd();
+
+                jsonResult = Common.StringHelper.UnicodeDencode(jsonResult);
+                JObject jobject = JObject.Parse(jsonResult);
+                int state = sqlHelper.ExecuteSql("insert into foodSafe (nameId,sort,name,alias,yunma,yunma_desc,big_pic,small_pic) values (" + jobject["id"] + "," + jobject["sort"] + ",'" + jobject["name"] + "','" + jobject["alias"] + "'," + jobject["yunma"] + ",'" + jobject["yunma_desc"] + "','" + jobject["big_pic"] + "','" + jobject["small_pic"] + "')");
+                j = i;
+            }
+
+            return Content("nameId:" + j);
+            //return Content(jsonResult);
+        }
+        #endregion
+
+        #region 获取FoodList
+        public ContentResult getFoodList()
+        {
+            string sort = request.Form["sort"].Trim();
+            DataSet ds = sqlHelper.GetDataSet("select * from foodSafe where sort=" + sort);
+            return Content(Common.JsonHelper.ToJson(ds.Tables[0]));
+
+        }
+        #endregion
+
+        #region updateRemind
+        public ContentResult updateRemind()
+        {
+            string week = request.Form["week"].Trim();
+            string day = request.Form["day"].Trim();
+
+            DataSet ds_1 = sqlHelper.GetDataSet("select top(1) babyPhysical,momPhysical from remindDailyData where week=" + week + " and day=" + day);
+            DataSet ds_2 = sqlHelper.GetDataSet("select top(1) checkTimes,checkWeek from pregnancyCheck where checkWeek>=" + week);
+            DataSet ds_3 = sqlHelper.GetDataSet("select top(1) nutritionRecipes from recipes where week=" + week + " and day=" + day);
+
+            DataTable table = ds_1.Tables[0];
+            table.Columns.Add("week");
+            table.Columns.Add("day");
+            table.Columns.Add("checkTimes");
+            table.Columns.Add("checkWeek");
+            table.Columns.Add("nutritionRecipes");
+
+            table.Rows[0]["week"] = week;
+            table.Rows[0]["day"] = day;
+            table.Rows[0]["checkTimes"] = ds_2.Tables[0].Rows[0]["checkTimes"];
+            table.Rows[0]["checkWeek"] = ds_2.Tables[0].Rows[0]["checkWeek"];
+            table.Rows[0]["nutritionRecipes"] = ds_3.Tables[0].Rows[0]["nutritionRecipes"];
+
+            return Content(Common.JsonHelper.ToJson(table));
+        }
+        #endregion
+
+        #region getItemList
+        public ContentResult getItemList()
+        {
+            string week = request.Form["week"].Trim();
+
+            DataSet ds_1 = sqlHelper.GetDataSet("select top(7) week,day,babyPhysical,momPhysical from remindDailyData where week=" + week);
+            DataSet ds_2 = sqlHelper.GetDataSet("select top(1) checkTimes,checkWeek from pregnancyCheck where checkWeek>=" + week);
+            DataSet ds_3 = sqlHelper.GetDataSet("select top(7) nutritionRecipes from recipes where week=" + week);
+
+            DataTable table = ds_1.Tables[0];
+            table.Columns.Add("checkTimes");
+            table.Columns.Add("checkWeek");
+            table.Columns.Add("nutritionRecipes");
+
+            for (int i = 0; i < 7; i++)
+            {
+                table.Rows[i]["checkTimes"] = ds_2.Tables[0].Rows[0]["checkTimes"];
+                table.Rows[i]["checkWeek"] = ds_2.Tables[0].Rows[0]["checkWeek"];
+                table.Rows[i]["nutritionRecipes"] = ds_3.Tables[0].Rows[i]["nutritionRecipes"];
+            }
+
+            return Content(Common.JsonHelper.ToJson(table));
+        }
+        #endregion
+
+        #region getCheckList
+        public ContentResult getCheckList()
+        {
+            string week = request.Form["week"].Trim();
+            string type = request.Form["type"].Trim();
+            DataSet ds = new DataSet();
+            switch (type)
+            {
+                case "incrase":
+                    ds = sqlHelper.GetDataSet("select top(1) * from pregnancyCheck where checkWeek>=" + week);
+                    break;
+                case "reduce":
+                    ds = sqlHelper.GetDataSet("select top(1) * from pregnancyCheck where checkWeek<=" + week + " order by checkWeek desc");
+                    break;
+            }
+            return Content(Common.JsonHelper.ToJson(ds.Tables[0]));
+        }
+        #endregion
+
+        #region searchFoodByName
+        public ContentResult searchFoodByName()
+        {
+            string keyWord = request.Form["keyWord"].Trim();
+            DataSet ds = sqlHelper.GetDataSet("select * from foodSafe where alias like '%" + keyWord + "%' or name like '%" + keyWord + "%'");
+            if (ds == null)
+                return Content("E");
+            else
+                return Content(Common.JsonHelper.ToJson(ds.Tables[0]));
+        }
+        #endregion
+
+        #region getQuestionList
+        public ContentResult getQuestionList()
+        {
+            DataSet ds = sqlHelper.GetDataSet("select * from questionList order by addTime desc");
+            if (ds == null)
+                return Content("E");
+            else
+                return Content(Common.JsonHelper.ToJson(ds.Tables[0]));
         }
         #endregion
     }
